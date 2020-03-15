734f3ca9dcf11cacd890b0fc6d08930625ba06ec
Andrew <48319785+isAloneWanderer@users.noreply.github.com>
2020-03-16 00:17:41 +0300

    design task-list fix

--- src/components/TaskListItem.jsx
+++ src/components/TaskListItem.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const TaskListItem = ({ index }) => (
-  <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+  <div className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3">
     <div className="card shadow-sm">
-      <div className="card-body">
-        <h5 className="card-title display-2 text-center">{index + 1}</h5>
-      </div>
-      <div className="card-footer d-flex justify-content-center">
-        <NavLink to={`${index}`} className="btn btn-info">Выбрать задачу</NavLink>
+      <div className="card-body d-flex justify-content-center">
+        <NavLink to={`${index}`} className="btn btn-info stretched-link px-3">{index + 1}</NavLink>
       </div>
     </div>
   </div>
