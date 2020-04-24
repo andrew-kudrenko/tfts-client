c0d1f05770aa35bd22b53274fdb94d946b8d0105
Andrew <48319785+isAloneWanderer@users.noreply.github.com>
2020-04-24 16:59:52 +0300

    build folder for deploy

---
+++ build/precache-manifest.0474d350c4445e923bee7029b3df5e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c828f0801ef8cc37a48413c64c2e758a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38a502747630754ab606",
+    "url": "/static/css/main.37df2711.chunk.css"
+  },
+  {
+    "revision": "dfacddb92b8fd94785b6",
+    "url": "/static/js/2.8b7abd59.chunk.js"
+  },
+  {
+    "revision": "a02c2e6156cb6e9e5c30be88d1ba3366",
+    "url": "/static/js/2.8b7abd59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a502747630754ab606",
+    "url": "/static/js/main.714dbf92.chunk.js"
+  },
+  {
+    "revision": "e4f4c0aaa1e39da0a036",
+    "url": "/static/js/runtime-main.aa2fb0d7.js"
+  }
+]);
